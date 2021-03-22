679af3210d89c60925256c02d08b0a175564f22f
Michal Cieslik <michal5100.mc@gmail.com>
2021-03-22 22:18:01 +0100

    xd

--- src/Client/MyClient.java
+++ src/Client/MyClient.java
@@ -12,7 +12,7 @@ public class MyClient {
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             dout.writeUTF("Hello server");
             dout.flush();
-            dout.close();//JD
+            dout.close();//JDD
             s.close();
         } catch (UnknownHostException e) {
             e.printStackTrace();
